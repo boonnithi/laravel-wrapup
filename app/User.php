bd513160a885174db6276aaca3bb55516010b5c3
Boonnithi <boonnithi.c@gmail.com>
2018-07-10 01:14:07 +0700

    edit fillable

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'surname', 'nickname', 'email', 'password', 'dob', 'gender', 'biography', 'education', 'country', 'website'
     ];
 
     /**
