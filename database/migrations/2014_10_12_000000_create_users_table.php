314aca3c7d16abd0a90583648658ae2f4afc760d
Boonnithi <boonnithi.c@gmail.com>
2018-07-10 01:08:21 +0700

    add database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
+            $table->string('nickname')->nullable(); //some columns can be null because you can ignore it when register (when you register you can empty values in some columns).
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('dob');
+            $table->string('gender');
+            $table->string('biography')->nullable();
+            $table->string('education')->nullable();
+            $table->string('country')->nullable();
+            $table->string('website')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
